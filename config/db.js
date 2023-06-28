7d191eb14ced70eb06d04aecd1f280b53d6805ad
Abhishek <abhishekkumar9128138650@gmail.com>
2023-06-28 12:23:50 +0530

    Update db.js
    db.js updated

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const connectDB = async () => {
   try {
     // await mongoose.connect(process.env.MONGODB_URL);
-    await mongoose.connect("mongodb://127.0.0.1:27017/doctorapp");
+    await mongoose.connect(process.env.MONGODB_URL);
     // console.log(`mongodb connected ${mongoose.connection.host}`.bgGreen.white);
     console.log("Database connected".bgGreen.white);
   } catch (error) {
